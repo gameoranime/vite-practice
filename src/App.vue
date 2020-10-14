b8561fc4c24e07620e0d1b553a7bb3fea7d7edab
zengjiahao <250575102@qq.com>
2020-10-14 15:31:32 +0800

    提交项目

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+<img alt="Vue logo" src="./assets/logo.png" />
+<HelloWorld :msg="msg" />
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+    name: 'App',
+    components: {
+        HelloWorld
+    },
+    data() {
+        return {
+            msg: 'first time use this !'
+        }
+    }
+}
+</script>
